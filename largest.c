f570d9b220e0cc45e91ac0e315a05da2b6c016f5
sumankitha <d.sumankitha@s.amity.edu>
2025-03-08 13:46:10 +0530

    tittle

---
+++ largest.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+	int num1,num2,num3;
+	printf("enter the first number");
+		scanf("%d",&num1);
+		printf("enter the second number");
+		scanf("%d",&num2);
+		printf("enter the third number");
+		scanf("%d",&num3);
+		if(num 1>= num 2 && num 1>= num 3){
+			printf("the largest  number is 
